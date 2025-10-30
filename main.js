11189dc41c8c85642bacdc6ba093920e7a601b8e
harryscully <harry.scully@ugogroup.co.uk>
2025-10-30 17:22:21 +0000

    Render menu items with JS

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-import { menuArray } from "./data/data";
+import { menuArray } from "./data/data.js";
+
+function getMenuItems(arr) {
+    let menuItems = ''
+    
+    arr.forEach((item) => {
+        menuItems += 
+            `<div class="menu-item">
+                <p>${item.emoji}</p>
+                <div class='item-text'>
+                    <h3>${item.name}</h3>
+                    <p>${item.ingredients.join(', ')}</p>
+                    <p>£${item.price}</p>
+                </div>
+                <p class="add-item">+</p>
+            </div>`
+        } 
+    )
+    
+    return menuItems
+}
+
+function renderMenuItems() {
+    document.getElementById("menu-items").innerHTML = getMenuItems(menuArray)
+}
+
+renderMenuItems()
