8be95456be3bf4bedf3bd947d32579aadbf48c67
harryscully <harry.scully@ugogroup.co.uk>
2025-10-31 11:02:54 +0000

    Add styling to menu items

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ function getMenuItems(arr) {
     arr.forEach((item) => {
         menuItems += 
             `<div class="menu-item">
-                <p>${item.emoji}</p>
+                <p class="item-emoji">${item.emoji}</p>
                 <div class='item-text'>
-                    <h3>${item.name}</h3>
-                    <p>${item.ingredients.join(', ')}</p>
-                    <p>£${item.price}</p>
+                    <h3 class="item-name">${item.name}</h3>
+                    <p class="item-ingreds">${item.ingredients.join(' | ')}</p>
+                    <p class="item-price">£${item.price}</p>
                 </div>
-                <p class="add-item">+</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dedede" class="add-item" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                </svg>
             </div>`
         } 
     )
